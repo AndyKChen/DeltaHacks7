5865da02b8b3daa9d888809d5da9b08b5c077fdf
Matthew Wang <mslwang2001@gmail.com>
2021-03-06 14:29:42 -0500

    Add Navbar

--- client/src/Pages/Quiz.js
+++ client/src/Pages/Quiz.js
@@ -1,9 +1,11 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
+import Navigation from '../Components/Navigation/Navigation';
 
 const Quiz = () => {
   return (
     <div>
+      <Navigation />
       <Button variant="contained" color="primary">
         Primary
       </Button>
