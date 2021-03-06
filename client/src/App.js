e19ad985271dd5a2f9dbf68e26cd9495796cab95
Andy Chen <andychen2325@gmail.com>
2021-03-06 12:42:22 -0500

    added-ml

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,10 @@ import fullscreen from './Icons/fullscreen.svg'
 import minimize from './Icons/minimize.svg'
 import ringtone from './Sounds/ringtone.mp3'
 
+var identity = 0;
+var classes = []; // list of classes
+var text = ""
+
 const Watermark = React.lazy(()=>import('./Components/Watermark/Watermark'))
 
 const ringtoneSound = new Howl({
@@ -81,6 +85,122 @@ function App() {
     </main>
     <Footer/>
   </>
+
+  const start = async () => {
+    const trainingCards = document.getElementById("training-cards")
+    const predictions = document.getElementById("predictions")
+    const confidence = document.getElementById("confidence")
+
+    const createKNNClassifier = async () => {
+      console.log('Loading KNN Classifier');
+      return await window.knnClassifier.create();
+ };
+    const createMobileNetModel = async () => {
+      console.log('Loading Mobilenet Model');
+      return await window.mobilenet.load();
+    };
+    const createWebcamInput = async () => {
+      console.log('Loading Webcam Input');
+      const webcamElement = await document.getElementById('webcam');
+      return await window.tf.data.webcam(webcamElement);
+    };
+
+
+    const mobilenetModel = await createMobileNetModel();
+    const knnClassifierModel = await createKNNClassifier();
+    const webcamInput = await createWebcamInput();
+    var preloader = document.getElementById("loading");
+
+    function preLoader() {
+        preloader.style.display = 'none';
+    };
+    preLoader()
+  
+    const addClass = () => {
+      const inputClassName = document.getElementById("inputClassName")
+
+      let Classname = inputClassName.value
+      const found = classes.some(el => el.name === Classname);
+      if (!found) {
+          identity += 1
+          classes.push({ id: identity, name: Classname, count: 0 });
+      }
+
+
+      trainingCards.innerHTML += '<div class="newshifter"><div class="text-center"><h3>Class Name : <span>' + Classname + '</span></h3><h3>Images : <span id = "images-' + identity + '" >0</span></h3></div ><div><button class="dark btn-spread btn-shadow mr-5" id="' + identity + '">Add New Images <i class="fas fa-plus fa-1x"></i></button></div></div>'
+
+      document.getElementById(identity.toString()).addEventListener('click', () => addDatasetClass(identity));
+      inputClassName.value = ""
+      console.log(classes)
+    }
+
+    const initializeElements = () => {
+      const inputClassName = document.getElementById("inputClassName").value
+      document.getElementById('add-button').addEventListener('click', () => addClass(inputClassName));
+      // document.getElementById('btnSpeak').addEventListener('click', () => speak());
+      // document.getElementById('load_button').addEventListener('change', (event) => uploadModel(knnClassifierModel, event));
+      // document.getElementById('save_button').addEventListener('click', async () => downloadModel(knnClassifierModel));
+
+
+    };
+
+    const addDatasetClass = async (classId) => {
+
+    // Capture an image from the web camera.
+    const img = await webcamInput.capture();
+
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = mobilenetModel.infer(img, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    knnClassifierModel.addExample(activation, classId);
+
+    let classIndex = classes.findIndex(el => el.id === classId)
+    let currentCount = classes[classIndex].count
+    currentCount += 1
+    classes[classIndex].count = currentCount
+
+    var temp_id = 'images-' + classId.toString()
+    document.getElementById(temp_id).innerHTML = currentCount;
+
+    // Dispose the tensor to release the memory.
+    img.dispose();
+  };
+
+    const imageClassificationWithTransferLearningOnWebcam = async () => {
+      console.log("Machine Learning on the web is ready");
+      while (true) {
+          if (knnClassifierModel.getNumClasses() > 0) {
+                const img = await webcamInput.capture();
+
+                // Get the activation from mobilenet from the webcam.
+                const activation = mobilenetModel.infer(img, 'conv_preds');
+                // Get the most likely class and confidences from the classifier module.
+                const result = await knnClassifierModel.predictClass(activation);
+
+                //console.log(classes[result.label - 1].name)
+                try {
+                    predictions.innerHTML = classes[result.label - 1].name
+                    confidence.innerHTML = Math.floor(result.confidences[result.label] * 100)
+                    document.getElementById('change-prediction').click();
+                }
+                catch (err) {
+                    predictions.innerHTML = result.label - 1
+                    confidence.innerHTML = Math.floor(result.confidences[result.label] * 100)
+                }
+
+
+                // Dispose the tensor to release the memory.
+                img.dispose();
+          }
+          await window.tf.nextFrame();
+      }
+    };
+
+    await initializeElements();
+    await imageClassificationWithTransferLearningOnWebcam();
+  }
 
   useEffect(() => {
     socket.current = io.connect("/");
@@ -387,6 +507,10 @@ function App() {
     </span>
   }
 
+  window.onload = async () => {
+    await start();
+};
+
   return (
     <>
       <div style={{display: renderLanding()}}>
@@ -420,6 +544,48 @@ function App() {
           {fullscreenButton}
           {hangUp}
         </div>
+      </div>
+      <div>
+      <div id="video-grid"></div>
+      <div id="loading"></div>
+      <div className="row">
+
+        <div className="mycam">
+          <video hidden autoPlay playsInline muted id="webcam" className="cam"></video>
+          <div className="grey-bg">
+              <div className="row text-center">
+                  <h3>Prediction: <span id="predictions"></span></h3>
+                  <h3>Probability : <span id="confidence"></span> %</h3>
+                  <button hidden id="change-prediction"></button>
+              </div>
+          </div>
+        </div>
+        <div className="column flex-2-container">
+          <div>
+          <div className="model">
+              <button className="dark btn-lg btn-shadow " id="save_button">Download Model 
+              <i className="fas fa-download"></i>
+              </button>
+              <input id="load_button" className="fileinputs" type="file" accept=".json"></input>
+                <label htmlFor="upload-photo">Browse...</label> 
+              
+            </div>
+            <div className="add-class text-center">
+
+
+              <input id="inputClassName" type="text" placeholder="Enter Class Name Here" name="option" />
+              <button className="dark btn-lg btn-shadow mr-5" id="add-button">Add 
+              <i className="fas  fa-plus fa-1x"></i>
+              </button>
+                
+              </div>
+
+              <div id="training-cards">
+
+              </div>
+          </div>
+        </div>
+      </div>
       </div>
     </>
   )
