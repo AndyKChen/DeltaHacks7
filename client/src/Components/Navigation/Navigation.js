3e6cea962b87e212e5532762c765aecc14548a8f
Matthew Wang <mslwang2001@gmail.com>
2021-03-06 15:42:04 -0500

    Create rudimentary navbar

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,24 +1,56 @@
 import "../Navigation/Navigation.css";
+import {
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
 
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: "#FBFBFB"
+  },
+  title: {
+    flexGrow: 1,
+    color: "#FBFBFB"
+  },
+  navBar: {
+    "background-color": "#232323"
+  },
+  buttonText: {
+    color: "#FBFBFB",
+  },
+  button:{
+    border: "1px solid #FBFBFB",
+  },
+}));
 const Navigation = () => {
+  const classes = useStyles();
   return (
-    <header className="dropShadow">
-      <div className="headerWrapper">
-        <div className="headerContainer flex">
-          <div className="headerLogoLinkWrapper">
-            <div className="headerLogoLink">
-              <a href="/">
-                <div className="headerLogo flex flex-row">
-                  <div className="logoText">React app</div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+    <AppBar position="static" className={classes.navBar}>
+    <Toolbar>
+      <IconButton edge="start" className={classes.menuButton} color="#FBFBFB" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" className={classes.title}>
+        News
+      </Typography>
+      <Button className={classes.button}>
+        <Typography className={classes.buttonText}>
+        Login
+        </Typography>
+        </Button>
+    </Toolbar>
+  </AppBar>
   );
 };
 export default Navigation;
