139e851359688f41f1862d3a08c71ce95580d0c3
Matthew Wang <mslwang2001@gmail.com>
2021-03-06 16:51:20 -0500

    Modify protected navbar to be tab based

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import "../Navigation/Navigation.css";
+//import "../Navigation/Navigation.css";
 import {
   Button,
   AppBar,
@@ -7,9 +7,80 @@ import {
   Typography,
 
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import React from "react";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {BrowserRouter, Link} from 'react-router-dom';
+
+const AntTabs = withStyles({
+  root: {
+    borderBottom: '1px solid #e8e8e8',
+  },
+  indicator: {
+    backgroundColor: '#1890ff',
+  },
+})(Tabs);
+
+const AntTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(4),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      color: '#40a9ff',
+      opacity: 1,
+    },
+    '&$selected': {
+      color: '#1890ff',
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    '&:focus': {
+      color: '#40a9ff',
+    },
+  },
+  selected: {},
+}))((props) => <Tab disableRipple {...props} />);
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > span': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  },
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    color: '#fff',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))((props) => <Tab disableRipple {...props} />);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,26 +102,51 @@ const useStyles = makeStyles((theme) => ({
   },
   button:{
     border: "1px solid #FBFBFB",
+    marginRight: 10
   },
+    padding: {
+      padding: theme.spacing(3),
+    },
+    demo1: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    demo2: {
+      backgroundColor: '#232323',
+    },
+    ctr: {
+      margin: 'auto',
+    },
 }));
+
 const Navigation = () => {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <AppBar position="static" className={classes.navBar}>
-    <Toolbar>
-      <IconButton edge="start" className={classes.menuButton} color="#FBFBFB" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" className={classes.title}>
-        News
-      </Typography>
-      <Button className={classes.button}>
+    <BrowserRouter>
+        <AppBar position="static" className={classes.navBar}>
+          
+
+      <section className={classes.ctr}>
+      <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example">
+          <StyledTab label="Learn" to="/learn" component={Link}/>
+          <StyledTab label="Train" to="/train" component={Link}/>
+          <StyledTab label="Talk" to="/talk" component={Link}/>
+        </StyledTabs>
+      </section>
+      
+      {/* <Button className={classes.button}>
         <Typography className={classes.buttonText}>
-        Login
+          Login
         </Typography>
-        </Button>
-    </Toolbar>
+      </Button> */}
   </AppBar>
+    </BrowserRouter>
+
   );
 };
 export default Navigation;
