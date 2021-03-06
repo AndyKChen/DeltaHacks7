8f9a5bfa2fcef23f6700b115d74c8cae009d4d22
Matthew Wang <mslwang2001@gmail.com>
2021-03-06 18:42:19 -0500

    Add list for possible answers and query for information

--- client/src/Pages/Quiz.js
+++ client/src/Pages/Quiz.js
@@ -1,14 +1,30 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
 import Navigation from '../Components/Navigation/Navigation';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import axios from 'axios';
+
 
 const Quiz = () => {
+  const [question, setQuestion] = React.useState("");
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  React.useEffect( ()=> {
+    axios.get(BACKEND_URL + '/random-question').then((res) => {
+      console.log(res);
+      setQuestion(res.data["picture"]);
+    })
+  }, []);
   return (
     <div>
       <Navigation />
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
+      
+      <List>
+        <ListItem button><img src={question} /></ListItem>
+      </List>
+
+
+      
     </div>
   );
 };
