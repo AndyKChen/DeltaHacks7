92d5df03b32255d70c3c57c7068378deeea2c509
Kerry Liu <kerryliukerui@gmail.com>
2021-03-07 04:31:05 -0500

    Chnage the name/email showing in training

--- client/src/Components/VideoFrame/VideoFrame.js
+++ client/src/Components/VideoFrame/VideoFrame.js
@@ -1,9 +1,11 @@
 import Fab from '@material-ui/core/Fab';
 import React from 'react';
 import useStyles from './VideoFrame-jss';
+import { useAuth } from '../../Contexts/AuthContext'
 
 const VideoFrame = () => {
   const classes = useStyles();
+  const { currentUser } = useAuth()
   return (
     <div className={classes.videoFrame}>
       <video autoPlay playsInline muted id="webcam" className={classes.video}></video>
@@ -16,7 +18,7 @@ const VideoFrame = () => {
           className={classes.fab}
         >
           <div>
-            <div className={classes.name}>James Cahyadi</div>
+            <div className={classes.name}>{currentUser ? currentUser.email : "Not signed in"}</div>
             <div className={classes.prediction}>
               Prediction (<span id="confidence"></span>%): <span id="predictions"></span>
               <button hidden id="change-prediction"></button>
