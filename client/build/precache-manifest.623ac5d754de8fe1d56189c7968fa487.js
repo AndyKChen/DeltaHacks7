c31477e82cc69ab64dfd27168d4578f71873782c
Kerry Liu <kerryliukerui@gmail.com>
2021-03-06 23:17:22 -0500

    add build

---
+++ client/build/precache-manifest.623ac5d754de8fe1d56189c7968fa487.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc90f7d927d486d0e72329d877a4f7da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf758f9bbbfb5a542c3",
+    "url": "/static/css/main.5c793492.chunk.css"
+  },
+  {
+    "revision": "bdaf0fa220e971022ea3",
+    "url": "/static/js/2.b6c7311f.chunk.js"
+  },
+  {
+    "revision": "1bbad0fd9fcbc6cb78bf0a2b84ad49c4",
+    "url": "/static/js/2.b6c7311f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf758f9bbbfb5a542c3",
+    "url": "/static/js/main.828b4d5d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "feb48833bac136f91aab9b27583fb981",
+    "url": "/static/media/camera-stop.feb48833.svg"
+  },
+  {
+    "revision": "ff36203ce9be4c3b857c6e7648966b07",
+    "url": "/static/media/camera.ff36203c.svg"
+  },
+  {
+    "revision": "7722d32d9ff3e121881324b81968f255",
+    "url": "/static/media/fullscreen.7722d32d.svg"
+  },
+  {
+    "revision": "d3ae4842456ead735aecd20d34c2d80c",
+    "url": "/static/media/hang-up.d3ae4842.svg"
+  },
+  {
+    "revision": "bbec867b3bc32a35a52f068fe692f735",
+    "url": "/static/media/microphone-stop.bbec867b.svg"
+  },
+  {
+    "revision": "f375b0a7d2bf1c4703b4a98b5beeb1b8",
+    "url": "/static/media/microphone.f375b0a7.svg"
+  },
+  {
+    "revision": "612b180a63a0a715724db3a0be971688",
+    "url": "/static/media/minimize.612b180a.svg"
+  },
+  {
+    "revision": "0be3c651e1532de63056a20e2f341bde",
+    "url": "/static/media/ringtone.0be3c651.mp3"
+  },
+  {
+    "revision": "269beb7bd8d4931225aadb7103aa2be9",
+    "url": "/static/media/share.269beb7b.svg"
+  }
+]);
